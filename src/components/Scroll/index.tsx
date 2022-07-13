f9a9dd7a967d95d3ebe7af4788358173dfa827f2
brunod3vt3c <office.bruno.c.r@gmail.com>
2022-07-13 17:58:19 +0100

    change file scroll

--- src/components/Scroll/index.tsx
+++ src/components/Scroll/index.tsx
@@ -1,43 +1,9 @@
-import { Page, Scroll } from "framer";
+import React from 'react'
 
-const items = [0, 1, 2, 3, 4];
-
-export function Example() {
-    return (
-        <Page width={150} height={150} borderRadius={30} center directionLock>
-            <div
-                style={{
-                    width: 150,
-                    height: 150,
-                    borderRadius: 30,
-                    backgroundColor: "#fff"
-                }}
-            />
-            <Scroll width={150} height={150} borderRadius={30} directionLock>
-                {items.map((index) => {
-                    return (
-                        <div
-                            style={{
-                                width: 150,
-                                height: 70,
-                                borderRadius: 20,
-                                backgroundColor: "#fff",
-                                marginBottom:
-                                    index !== items.length - 1 ? 10 : 0
-                            }}
-                            key={index}
-                        />
-                    );
-                })}
-            </Scroll>
-            <div
-                style={{
-                    width: 150,
-                    height: 150,
-                    borderRadius: 30,
-                    backgroundColor: "#fff"
-                }}
-            />
-        </Page>
-    );
+function Scroll() {
+  return (
+    <div>Scroll</div>
+  )
 }
+
+export default Scroll
